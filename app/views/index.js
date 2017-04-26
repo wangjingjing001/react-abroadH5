bb6c61ff79bd353cacf24d65419de06c32309e14
xiongzhikai <xiongzhikai@rijin.com>
2017-04-26 16:41:14 +0800

    修改登录和注册页面

--- app/views/index.js
+++ app/views/index.js
@@ -2,6 +2,10 @@ module.exports = {
 	get App() { return require('./App.js')},
 	get IndexView() { return require('./IndexView.js');},
 	get LoginView() { return require('./LoginView.js');},
+	get RegisterView() { return require('./RegisterView.js');},
+	get CourseView() { return require('./CourseView.js');},
+	get VideoView() { return require('./VideoView.js');},
 	get SettingView() { return require('./SettingView.js');},
-	get NotFound404() { return require('./404.js');}
+	get NotFound404() { return require('./404.js');},
+
 };
